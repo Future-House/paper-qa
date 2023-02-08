994c858ff4d53764a2bcf857eaaa0d4d5b80560d
Andrew White <white.d.andrew@gmail.com>
2023-02-08 18:25:26 -0500

    Trying to make tests more robsut

--- paperqa/utils.py
+++ paperqa/utils.py
@@ -1,16 +1,24 @@
-import os
-import sys
+import math
+import string
 
 
-class HiddenPrints:
-    """Context manager to hide prints."""
+def maybe_is_text(s, thresh=2.5):
+    # Calculate the entropy of the string
+    entropy = 0
+    for c in string.printable:
+        p = s.count(c) / len(s)
+        if p > 0:
+            entropy += -p * math.log2(p)
 
-    def __enter__(self):
-        """Open file to pipe stdout to."""
-        self._original_stdout = sys.stdout
-        sys.stdout = open(os.devnull, "w")
+    # Check if the entropy is within a reasonable range for text
+    if entropy > thresh:
+        return True
+    return False
 
-    def __exit__(self, *_):
-        """Close file that stdout was piped to."""
-        sys.stdout.close()
-        sys.stdout = self._original_stdout
+
+def strings_similarity(s1, s2):
+    # break the strings into words
+    s1 = set(s1.split())
+    s2 = set(s2.split())
+    # return the similarity ratio
+    return len(s1.intersection(s2)) / len(s1.union(s2))
