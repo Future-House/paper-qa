7f0a3b742e3121ec61242e3a8031166728521a52
Andrew White <white.d.andrew@gmail.com>
2023-02-15 00:26:12 -0500

    Added raw passages and LLM options

--- paperqa/qaprompts.py
+++ paperqa/qaprompts.py
@@ -1,8 +1,6 @@
 import langchain.prompts as prompts
-import langchain.chains as chains
-import langchain.llms as llms
 
-_distill_prompt = prompts.PromptTemplate(
+summary_prompt = prompts.PromptTemplate(
     input_variables=["question", "context_str"],
     template="Provide relevant information that will help answer a question from the context below. "
     "Summarize the information in an unbiased tone. Use direct quotes "
@@ -15,21 +13,9 @@ _distill_prompt = prompts.PromptTemplate(
     "Question: {question}\n"
     "Relevant Information Summary:",
 )
-distill_chain = chains.LLMChain(
-    llm=llms.OpenAI(temperature=0.1), prompt=_distill_prompt
-)
 
-_query_prompt = prompts.PromptTemplate(
-    input_variables=["question"],
-    template="I would like to find scholarly papers to answer this question: {question}. "
-    'A search query that would bring up papers related to this answer would be: "',
-)
-query_chain = chains.LLMChain(
-    llm=llms.OpenAI(temperature=0.05, model_kwargs={"stop": ['"']}),
-    prompt=_query_prompt,
-)
 
-_qa_prompt = prompts.PromptTemplate(
+qa_prompt = prompts.PromptTemplate(
     input_variables=["question", "context_str", "length"],
     template="Write a comprehensive answer ({length}) "
     "for the question below solely based on the provided context. "
@@ -46,13 +32,11 @@ _qa_prompt = prompts.PromptTemplate(
     "Question: {question}\n"
     "Answer: ",
 )
-qa_chain = chains.LLMChain(llm=llms.OpenAI(temperature=0.1), prompt=_qa_prompt)
 
-_edit_prompt = prompts.PromptTemplate(
+edit_prompt = prompts.PromptTemplate(
     input_variables=["question", "answer"],
     template="The original question is: {question} "
     "We have been provided the following answer: {answer} "
     "Part of it may be truncated, please edit the answer to make it complete. "
     "If it appears to be complete, repeat it unchanged.\n\n",
 )
-edit_chain = chains.LLMChain(llm=llms.OpenAI(temperature=0.1), prompt=_edit_prompt)
--- tests/test_paperqa.py
+++ tests/test_paperqa.py
@@ -3,6 +3,7 @@ import requests
 import os
 import pickle
 from paperqa.utils import strings_similarity
+from langchain.llms import OpenAI
 
 
 def test_maybe_is_text():
@@ -11,7 +12,7 @@ def test_maybe_is_text():
     )
     assert not paperqa.maybe_is_text("\\C0\\C0\\B1\x00")
     # get front page of wikipedia
-    r = requests.get("https://en.wikipedia.org/wiki/Main_Page")
+    r = requests.get("https://en.wikipedia.org/wiki/National_Flag_of_Canada_Day")
     assert paperqa.maybe_is_text(r.text)
 
     # now force it to contain lots of weird encoding
@@ -23,9 +24,10 @@ def test_docs():
     doc_path = "example.txt"
     with open(doc_path, "w", encoding="utf-8") as f:
         # get front page of wikipedia
-        r = requests.get("https://en.wikipedia.org/wiki/Main_Page")
+        r = requests.get("https://en.wikipedia.org/wiki/National_Flag_of_Canada_Day")
         f.write(r.text)
-    docs = paperqa.Docs()
+    llm = OpenAI(temperature=0.1, model_name="text-ada-001")
+    docs = paperqa.Docs(llm=llm)
     docs.add(doc_path, "WikiMedia Foundation, 2023, Accessed now")
     assert docs.docs[doc_path]["key"] == "Wiki2023"
     os.remove(doc_path)
@@ -37,7 +39,7 @@ def test_evidence():
         # get wiki page about politician
         r = requests.get("https://en.wikipedia.org/wiki/Frederick_Bates_(politician)")
         f.write(r.text)
-    docs = paperqa.Docs()
+    docs = paperqa.Docs(llm=OpenAI(temperature=0.1, model_name="text-curie-001"))
     docs.add(doc_path, "WikiMedia Foundation, 2023, Accessed now")
     evidence, _ = docs.get_evidence(
         "For which state was he a governor", k=1, max_sources=1
@@ -52,7 +54,7 @@ def test_query():
         # get wiki page about politician
         r = requests.get("https://en.wikipedia.org/wiki/Frederick_Bates_(politician)")
         f.write(r.text)
-    docs = paperqa.Docs()
+    docs = paperqa.Docs(llm=OpenAI(temperature=0.1, model_name="text-ada-001"))
     docs.add(doc_path, "WikiMedia Foundation, 2023, Accessed now")
     answer = docs.query("What is Frederick Bates's greatest accomplishment?")
     print(answer)
@@ -63,17 +65,21 @@ def test_docs_pickle():
     doc_path = "example.txt"
     with open(doc_path, "w", encoding="utf-8") as f:
         # get front page of wikipedia
-        r = requests.get("https://en.wikipedia.org/wiki/Main_Page")
+        r = requests.get("https://en.wikipedia.org/wiki/National_Flag_of_Canada_Day")
         f.write(r.text)
-    docs = paperqa.Docs()
+    docs = paperqa.Docs(llm=OpenAI(temperature=0.0, model_name="text-babbage-001"))
     docs.add(doc_path, "WikiMedia Foundation, 2023, Accessed now")
     docs_pickle = pickle.dumps(docs)
     docs2 = pickle.loads(docs_pickle)
     assert len(docs.docs) == len(docs2.docs)
     assert (
         strings_similarity(
-            docs.get_evidence("What is the date?", k=1, max_sources=1)[0],
-            docs2.get_evidence("What is the date?", k=1, max_sources=1)[0],
+            docs.get_evidence("What date is flag day in Canada?", k=1, max_sources=1)[
+                0
+            ],
+            docs2.get_evidence("What date is flag day in Canada?", k=1, max_sources=1)[
+                0
+            ],
         )
         > 0.75
     )
@@ -86,7 +92,7 @@ def test_bad_context():
         # get wiki page about politician
         r = requests.get("https://en.wikipedia.org/wiki/Frederick_Bates_(politician)")
         f.write(r.text)
-    docs = paperqa.Docs()
+    docs = paperqa.Docs(llm=OpenAI(model_name="text-davinci-003"))
     docs.add(doc_path, "WikiMedia Foundation, 2023, Accessed now")
     answer = docs.query("What year was Barack Obama born greatest accomplishment?")
     assert (
@@ -102,7 +108,7 @@ def test_repeat_keys():
         # get wiki page about politician
         r = requests.get("https://en.wikipedia.org/wiki/Frederick_Bates_(politician)")
         f.write(r.text)
-    docs = paperqa.Docs()
+    docs = paperqa.Docs(llm=OpenAI(temperature=0.0, model_name="text-ada-003"))
     docs.add(doc_path, "WikiMedia Foundation, 2023, Accessed now")
     try:
         docs.add(doc_path, "WikiMedia Foundation, 2023, Accessed now")
@@ -129,7 +135,7 @@ def test_repeat_keys():
 def test_pdf_reader():
     tests_dir = os.path.dirname(os.path.abspath(__file__))
     doc_path = os.path.join(tests_dir, "paper.pdf")
-    docs = paperqa.Docs()
+    docs = paperqa.Docs(llm=OpenAI(temperature=0.0, model_name="text-curie-001"))
     docs.add(doc_path, "Wellawatte et al, XAI Review, 2023")
-    answer = docs.query("What is LIME?")
-    assert "Local Interpretable Model-Agnostic Explanations" in answer.answer
+    answer = docs.query("Are counterfactuals actionable (yes/no)?")
+    assert "yes" in answer.answer or "Yes" in answer.answer
