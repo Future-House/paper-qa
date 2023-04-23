013be0c59ef47d1dcba4aeabecd6ff445d432baa
Sway Ducky <swayducky@gmail.com>
2023-04-23 12:32:34 -0700

    test

--- test.py
+++ test.py
@@ -4,25 +4,29 @@ load_dotenv()
 import code
 import os
 from paperqa import Docs
-
-docs = Docs()
 import re
+
+d = Docs()
 
 IGNORE_DIRS = set(['venv', 'node_modules'])
 RX_IGNORE = re.compile(r'(^\..*)|(.*\.pyc$)')
 for code_dir in ['.']:
     for dirpath, dirnames, filenames in os.walk(code_dir, topdown=True):
-        dirnames[:] = [d for d in dirnames if d not in IGNORE_DIRS and not d.startswith('.')]
+        dirnames[:] = [x for x in dirnames if x not in IGNORE_DIRS and not x.startswith('.')]
         for file in filenames:
             if RX_IGNORE.match(file):
                 continue
             path = os.path.join(dirpath, file)
             print("Adding", path)
-            docs.add(path, citation=path, key=path)
+            d.add(path, citation=path, key=path)
 
+a = d.query("What does this codebase do?")
+print(len(a.contexts), 'contexts')
+print('Cost:', a.cost, 'Tokens:', a.tokens)
+print(a.formatted_answer)
 
 code.interact(local=locals(), banner="""
 Welcome to the PaperQA REPL! Try this:
-  answer = docs.query("What manufacturing challenges are unique to bispecific antibodies?")
-  print(answer.formatted_answer)
+  a = d.query("What manufacturing challenges are unique to bispecific antibodies?")
+  print(a.formatted_answer)
 """)
