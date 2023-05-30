0b45bf39aba1dd3f9e1613a48a3b51d5dc13b3ce
Andrew White <white.d.andrew@gmail.com>
2023-05-30 10:20:45 -0400

    Fixed errors on key filters

--- paperqa/docs.py
+++ paperqa/docs.py
@@ -27,6 +27,7 @@ from .qaprompts import (
     search_prompt,
     select_paper_prompt,
     summary_prompt,
+    get_score,
 )
 from .readers import read_doc
 from .types import Answer, Context
@@ -373,11 +374,12 @@ class Docs:
             docs = self._faiss_index.similarity_search(
                 answer.question, k=_k, fetch_k=5 * _k
             )
+        # ok now filter
+        if key_filter is not None:
+            docs = [doc for doc in docs if doc.metadata["dockey"] in key_filter][:k]
 
         async def process(doc):
             if doc.metadata["dockey"] in self._deleted_keys:
-                return None, None
-            if key_filter is not None and doc.metadata["dockey"] not in key_filter:
                 return None, None
             # check if it is already in answer (possible in agent setting)
             if doc.metadata["key"] in [c.key for c in answer.contexts]:
@@ -408,8 +410,10 @@ class Docs:
                 citation=doc.metadata["citation"],
                 context=context,
                 text=doc.page_content,
+                score=get_score(context),
             )
             if "not applicable" not in c.context.casefold():
+                print(c.score)
                 return c, callbacks[0]
             return None, None
 
@@ -423,7 +427,7 @@ class Docs:
         contexts = [c for c, _ in results if c is not None]
         if len(contexts) == 0:
             return answer
-        contexts = sorted(contexts, key=lambda x: len(x.context), reverse=True)
+        contexts = sorted(contexts, key=lambda x: x.score, reverse=True)
         contexts = contexts[:max_sources]
         # add to answer (if not already there)
         keys = [c.key for c in answer.contexts]
@@ -511,11 +515,12 @@ class Docs:
         if answer is None:
             answer = Answer(query)
         if len(answer.contexts) == 0:
-            if key_filter or (key_filter is None and len(self.docs) > 5):
+            if key_filter or (key_filter is None and len(self.docs) > max_sources):
                 callbacks = [OpenAICallbackHandler()] + get_callbacks("filter")
                 keys = await self.adoc_match(answer.question, callbacks=callbacks)
                 answer.tokens += callbacks[0].total_tokens
                 answer.cost += callbacks[0].total_cost
+                key_filter = True if len(keys) > 0 else False
             answer = await self.aget_evidence(
                 answer,
                 k=k,
@@ -544,8 +549,8 @@ class Docs:
             answer.tokens += cb.total_tokens
             answer.cost += cb.total_cost
         # it still happens lol
-        if "(Foo2012)" in answer_text:
-            answer_text = answer_text.replace("(Foo2012)", "")
+        if "(Example2012)" in answer_text:
+            answer_text = answer_text.replace("(Example2012)", "")
         for c in contexts:
             key = c.key
             text = c.context
