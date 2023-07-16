97acdcb8f6990c93dfbff4d9791700136d92f91c
Jeffrey Cheng <jeff.s.cheng@gmail.com>
2023-07-16 17:22:07 -0400

    added rag endpoint

--- app.py
+++ app.py
@@ -1,15 +1,13 @@
-# app_paper_qa.py
-
-from flask import Flask, request, jsonify
+from flask import Flask, request
+import json
 
 app = Flask(__name__)
 
 @app.route('/ask_rag', methods=['POST'])
 def ask_rag():
-    chat_log = request.get_json(force=True)  # extract the chat log from the POST request
-    question = chat_log[-1]['user'] if chat_log else ""
-    response = f"Response to: {question}"
-    return jsonify(response)
+    chat_log = request.get_json()
+    last_question = chat_log[-1]['user'] if chat_log else ''
+    return f"Response to: {last_question}", 200
 
-if __name__ == '__main__':
-    app.run(port=5001)
+if __name__ == "__main__":
+    app.run(debug=True, port=5001)
