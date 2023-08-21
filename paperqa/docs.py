0fe94f70199b2955f99a294808da1cc0aa75b74b
dustyposa <zmzposa@outlook.com>
2023-08-21 18:16:08 +0800

    :wrench: remove empty line

--- paperqa/docs.py
+++ paperqa/docs.py
@@ -283,7 +283,6 @@ class Docs(BaseModel, arbitrary_types_allowed=True, smart_union=True):
         self.deleted_dockeys.add(dockey)
         self.texts = list(filter(lambda x: x.doc.dockey != dockey, self.texts))
 
-
     async def adoc_match(
         self, query: str, k: int = 25, get_callbacks: CallbackFactory = lambda x: None
     ) -> Set[DocKey]:
