104a652fd498e6bd995512089209d186d56bc3e2
James Braza <jamesbraza@gmail.com>
2024-03-07 14:58:54 -0800

    Added Doc.__hash__, with test (#244)

--- tests/test_paperqa.py
+++ tests/test_paperqa.py
@@ -641,21 +641,31 @@ def test_llmresult_callbacks():
     assert len(my_results) > 1
 
 
-def test_duplicate():
+def test_duplicate() -> None:
+    """Check Docs doesn't store duplicates, while checking nonduplicate docs are stored."""
     docs = Docs()
     assert docs.add_url(
         "https://en.wikipedia.org/wiki/Frederick_Bates_(politician)",
         citation="WikiMedia Foundation, 2023, Accessed now",
-        dockey="test",
+        dockey="test1",
     )
     assert (
         docs.add_url(
             "https://en.wikipedia.org/wiki/Frederick_Bates_(politician)",
             citation="WikiMedia Foundation, 2023, Accessed now",
-            dockey="test",
+            dockey="test1",
         )
         is None
     )
+    assert len(docs.docs) == 1, "Should have added only one document"
+    assert docs.add_url(
+        "https://en.wikipedia.org/wiki/National_Flag_of_Canada_Day",
+        citation="WikiMedia Foundation, 2023, Accessed now",
+        dockey="test2",
+    )
+    assert (
+        len(set(docs.docs.values())) == 2
+    ), "Unique documents should be hashed as unique"
 
 
 def test_custom_embedding():
