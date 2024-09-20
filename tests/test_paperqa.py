bd5d2628e93c01084b677a4bc7c71737d623547c
James Braza <jamesbraza@gmail.com>
2024-09-20 13:29:53 -0700

    Fixed mypy ignores in test_pdf_reader_match_doc_details

--- tests/test_paperqa.py
+++ tests/test_paperqa.py
@@ -10,7 +10,15 @@ import httpx
 import numpy as np
 import pytest
 
-from paperqa import Answer, Doc, Docs, NumpyVectorStore, Settings, print_callback
+from paperqa import (
+    Answer,
+    Doc,
+    DocDetails,
+    Docs,
+    NumpyVectorStore,
+    Settings,
+    print_callback,
+)
 from paperqa.clients import CrossrefProvider
 from paperqa.core import llm_parse_json
 from paperqa.llms import (
@@ -823,16 +831,18 @@ def test_pdf_reader_match_doc_details(stub_data_dir: Path) -> None:
         "41f786fcc56d27ff0c1507153fae3774",  # From file contents
         "5300ef1d5fb960d7",  # Or from crossref data
     }
+    assert isinstance(doc_details, DocDetails)
     # note year is unknown because citation string is only parsed for authors/title/doi
     # AND we do not request it back from the metadata sources
     assert doc_details.docname == "wellawatteUnknownyearaperspectiveon"
-    assert set(doc_details.authors) == {  # type: ignore[attr-defined]
+    assert doc_details.authors
+    assert set(doc_details.authors) == {
         "Geemi P. Wellawatte",
         "Heta A. Gandhi",
         "Aditi Seshadri",
         "Andrew D. White",
     }
-    assert doc_details.doi == "10.26434/chemrxiv-2022-qfv02"  # type: ignore[attr-defined]
+    assert doc_details.doi == "10.26434/chemrxiv-2022-qfv02"
     answer = docs.query("Are counterfactuals actionable? [yes/no]")
     assert "yes" in answer.answer or "Yes" in answer.answer
 
