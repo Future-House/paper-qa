683e57436f3242c6860e1b779fb9cf20a8dbf85c
joaquinpolonuer <jtpolonuer@gmail.com>
2025-01-31 12:50:01 -0800

    Use relative paths instead of absolute

--- gradable.py
+++ gradable.py
@@ -10,19 +10,19 @@ from paperqa.agents.task import LFRQATaskDataset
 
 async def evaluate() -> None:
     settings = Settings()
+    settings.agent.index.name = "lfrqa_science_docs"
     settings.agent.index.paper_directory = (
-        "/Users/joaquinpolonuer/Documents/rag-qa-arena/lfrqa/science_docs_for_paperqa"
+        "rag-qa-benchmarking/lfrqa/science_docs_for_paperqa"
     )
     settings.agent.index.index_directory = (
-        "/Users/joaquinpolonuer/Documents/rag-qa-arena/lfrqa/science_docs_for_paperqa_index"
+        "rag-qa-benchmarking/lfrqa/science_docs_for_paperqa_index"
     )
     settings.agent.index.manifest_file = "manifest.csv"
     settings.agent.agent_llm = "gpt-4o"
-    # settings.agent.search_count = 15
 
     settings.parsing.use_doc_details = False
 
-    dataset = LFRQATaskDataset(data_path="/Users/joaquinpolonuer/Documents/rag-qa-arena/lfrqa/questions.csv", settings=settings)
+    dataset = LFRQATaskDataset(data_path="rag-qa-benchmarking/lfrqa/questions.csv", settings=settings)
     metrics_callback = MeanMetricsCallback(eval_dataset=dataset)
 
     evaluator = Evaluator(
--- main.py
+++ main.py
@@ -2,17 +2,15 @@ from paperqa import Settings, ask
 
 
 settings = Settings()
-settings.agent.index.paper_directory = f"/Users/joaquinpolonuer/Documents/rag-qa-arena/lfrqa/science_docs_for_paperqa"
-settings.agent.index.index_directory = f"/Users/joaquinpolonuer/Documents/rag-qa-arena/lfrqa/science_docs_for_paperqa_index"
+settings.agent.index.name = "lfrqa_science_docs"
+settings.agent.index.paper_directory = f"rag-qa-benchmarking/lfrqa/science_docs_for_paperqa"
+settings.agent.index.index_directory = f"rag-qa-benchmarking/lfrqa/science_docs_for_paperqa_index"
 
 settings.agent.index.manifest_file = "manifest.csv"
-# settings.agent.search_count = 15
 
 settings.parsing.use_doc_details = False
 settings.parsing.defer_embedding = True
-settings.agent.index.concurrency = 1000
-
-# settings.prompts.qa.replace('If the context provides insufficient information reply "I cannot answer."', "")
+settings.agent.index.concurrency = 30000
 
 answer_response = (
     ask(
