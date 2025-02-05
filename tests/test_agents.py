8b5a0490502060835be6391c2540cd0ee9349d2e
Philipp SL Schäfer <philipp.schaefer98@icloud.com>
2025-02-05 15:42:23 +0100

    forgot to use raw string in regex

--- tests/test_agents.py
+++ tests/test_agents.py
@@ -1083,9 +1083,9 @@ async def test_search_pagination(agent_test_settings: Settings) -> None:
 @pytest.mark.asyncio
 async def test_empty_index_without_index_rebuild(agent_test_settings: Settings):
     """Test that empty index and `rebuild_index=False` lead to a RuntimeError."""
-    agent_test_settings.agent = AgentSettings(index=IndexSettings()) # empty index
+    agent_test_settings.agent = AgentSettings(index=IndexSettings())  # empty index
     agent_test_settings.agent.rebuild_index = False
-    with pytest.raises(RuntimeError, match=f"Index .* was empty, please rebuild it."):
+    with pytest.raises(RuntimeError, match=r"Index .* was empty, please rebuild it."):
         await agent_query(
             query="Are COVID-19 vaccines effective?",
             settings=agent_test_settings,
