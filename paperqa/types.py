c3b3be6c285cfe8c68a7110019f4d465f0f40768
joaquinpolonuer <jtpolonuer@gmail.com>
2025-02-07 15:42:30 -0800

    previous approach didn't work so trying setting overwrite_citation everywhere its necessary

--- paperqa/types.py
+++ paperqa/types.py
@@ -612,9 +612,7 @@ class DocDetails(Doc):
     @model_validator(mode="before")
     @classmethod
     def validate_all_fields(cls, data: Mapping[str, Any]) -> dict[str, Any]:
-        if not data.get("overwrite_citation_from_metadata"):
-            data["overwrite_citation_from_metadata"] = data.get("overwrite_fields_from_metadata", True)
-        elif isinstance(data.get("overwrite_citation_from_metadata"), str):
+        if isinstance(data.get("overwrite_citation_from_metadata"), str):
             if data.get("overwrite_citation_from_metadata", "").lower() in {
                 "0",
                 "false",
