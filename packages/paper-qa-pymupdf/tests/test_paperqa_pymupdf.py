5910427b5bec78311266086865a4b1cf1a975813
James Braza <jamesbraza@gmail.com>
2025-07-23 14:46:22 -0700

    Beefed up PyMuPDF test_parse_pdf_to_pages a bit

--- packages/paper-qa-pymupdf/tests/test_paperqa_pymupdf.py
+++ packages/paper-qa-pymupdf/tests/test_paperqa_pymupdf.py
@@ -1,5 +1,10 @@
 from pathlib import Path
 
+import pymupdf
+import pytest
+from paperqa.readers import PDFParserFn
+from paperqa.utils import ImpossibleParsingError
+
 from paperqa_pymupdf import parse_pdf_to_pages
 
 REPO_ROOT = Path(__file__).parents[3]
@@ -7,6 +12,8 @@ STUB_DATA_DIR = REPO_ROOT / "tests" / "stub_data"
 
 
 def test_parse_pdf_to_pages() -> None:
+    assert isinstance(parse_pdf_to_pages, PDFParserFn)
+
     filepath = STUB_DATA_DIR / "pasa.pdf"
     parsed_text = parse_pdf_to_pages(filepath, use_block_parsing=True)
     assert isinstance(parsed_text.content, dict)
@@ -14,4 +21,19 @@ def test_parse_pdf_to_pages() -> None:
     assert (
         "Abstract\n\nWe introduce PaSa, an advanced Paper Search"
         "\nagent powered by large language models."
-    ) in parsed_text.content["1"]
+    ) in parsed_text.content["1"], "Block parsing failed to handle abstract"
+
+    # Check Figure 1
+    p2_text = parsed_text.content["2"]
+    assert "Figure 1" in p2_text, "Expected Figure 1 title"
+    assert "Crawler" in p2_text, "Expected Figure 1 contents"
+
+    # Check metadata
+    (parsing_library,) = parsed_text.metadata.parsing_libraries
+    assert pymupdf.__name__ in parsing_library
+    assert parsed_text.metadata.parse_type == "pdf"
+
+
+def test_page_size_limit_denial() -> None:
+    with pytest.raises(ImpossibleParsingError, match="char limit"):
+        parse_pdf_to_pages(STUB_DATA_DIR / "paper.pdf", page_size_limit=10)  # chars
