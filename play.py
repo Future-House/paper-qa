e8f84555b929f80a00e45e6e4281fa84f677c621
James Braza <jamesbraza@gmail.com>
2025-08-12 13:31:43 -0700

    Added play script to test direct httpx

---
+++ play.py
@@ -0,0 +1,15 @@
+import httpx
+
+url = "https://clinicaltrials.gov/api/v2/studies?query.term=Covid-19+vaccines&fields=NCTId%2COfficialTitle&pageSize=1000&countTotal=true&sort=%40relevance"
+
+headers = {
+    "Accept": "*/*",
+    "Connection": "keep-alive",
+    "Host": "clinicaltrials.gov",
+    "User-Agent": "python-httpx/0.28.1",
+}
+
+with httpx.Client(http2=False) as client:
+    response = client.get(url, headers=headers)
+    print(f"Status: {response.status_code}")
+    print(f"Text: {response.text}")
