302d6e4da4ed5c78477ab8697ee11def6b2cc4a9
James Braza <jamesbraza@gmail.com>
2025-08-12 11:37:15 -0700

    Tried verify=False

--- src/paperqa/sources/clinical_trials.py
+++ src/paperqa/sources/clinical_trials.py
@@ -255,7 +255,11 @@ async def add_clinical_trials_to_docs(
             Total number of trials found, number of trials added, and error message if any.
     """
     # Cookies are not needed
-    _client = httpx.AsyncClient(timeout=10.0, http2=False) if client is None else client
+    _client = (
+        httpx.AsyncClient(timeout=10.0, verify=False)  # noqa: S501
+        if client is None
+        else client
+    )
     for k in list(_client.headers.keys()):
         _client.headers.pop(k)
 
