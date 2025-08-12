e2d5b519e9ed60123fc913af9cd14304af9f5ba1
James Braza <jamesbraza@gmail.com>
2025-08-12 13:57:50 -0700

    Testing brotli, also ssl context

--- play.py
+++ play.py
@@ -1,7 +1,8 @@
+import ssl
+
 import httpx
 
 url = "https://clinicaltrials.gov/api/v2/studies?query.term=Covid-19+vaccines&fields=NCTId%2COfficialTitle&pageSize=1000&countTotal=true&sort=%40relevance"
-
 headers = {
     "Accept": "*/*",
     "Connection": "keep-alive",
@@ -9,7 +10,11 @@ headers = {
     "User-Agent": "python-httpx/0.28.1",
 }
 
-with httpx.Client(http2=False) as client:
+context = ssl.create_default_context()
+context.minimum_version = ssl.TLSVersion.TLSv1_2
+context.maximum_version = ssl.TLSVersion.TLSv1_2
+
+with httpx.Client(verify=context, http2=False) as client:
     response = client.get(url, headers=headers)
     print(f"Status: {response.status_code}")
     print(f"Text: {response.text}")
