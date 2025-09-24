2c5e82f444a77fc2152bb96245534c22813b3b3c
Andrew White <white.d.andrew@gmail.com>
2025-09-24 09:08:16 -0700

    Upstreaming prompt change

--- src/paperqa/prompts.py
+++ src/paperqa/prompts.py
@@ -113,10 +113,12 @@ summary_json_system_prompt = (
     " Respond with the following JSON format:"
     '\n\n{{\n  "summary": "...",\n  "relevance_score": "..."\n  "used_images"\n}}'
     "\n\nwhere `summary` is relevant information from the text - {summary_length} words."
-    " `relevance_score` is an integer 1-10 for the relevance of `summary` to the question."
+    " `relevance_score` is an integer 0-10 for the relevance of `summary` to the question."
     " `used_images` is a boolean flag indicating"
     " if any images present in a multimodal message were used,"
     " and if no images were present it should be false."
+    "\n\nThe excerpt may or may not contain relevant information. If not, leave `summary` empty, "
+    "and make `relevance_score` be 0."
 )
 
 env_system_prompt = (
