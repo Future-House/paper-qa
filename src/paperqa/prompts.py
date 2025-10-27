3189dcdbf3cbd863a5d95dd38232fac71de23b0d
James Braza <jamesbraza@gmail.com>
2025-10-27 09:41:35 -0700

    Simplified prompting around Markdown table text

--- src/paperqa/prompts.py
+++ src/paperqa/prompts.py
@@ -16,7 +16,7 @@ summary_prompt = (
 text_with_tables_prompt_template = (
     "{text}\n\n------------\n\nMarkdown tables from {citation}."
     " If the markdown is poorly formatted, defer to the images"
-    "\n\n------------\n\n{tables}"
+    "\n\n{tables}"
 )
 
 summary_json_prompt = (
