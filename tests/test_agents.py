f4d81b56ea19d080330d9007db1fed56fe94d517
James Braza <jamesbraza@gmail.com>
2025-11-05 23:05:29 -0800

    Further adjusting expected counts in test_get_directory_index

--- tests/test_agents.py
+++ tests/test_agents.py
@@ -160,9 +160,10 @@ async def test_get_directory_index(
             ),
         ):
             index = await get_directory_index(settings=agent_test_settings)
-        # Subtract 4 for the removed obama.txt, dummy.docx, dummy.pptx, and dummy.xlsx files,
+        # Subtract 5 for the removed obama.txt,
+        # dummy.docx, dummy_jap.docx, dummy.pptx, and dummy.xlsx files,
         # and another 1 for the filtered out flag_day.html
-        assert len(await index.index_files) == len(path_to_id) - 4 - 1
+        assert len(await index.index_files) == len(path_to_id) - 5 - 1
         mock_aadd.assert_not_awaited(), "Expected we didn't re-add files"
 
         # Note let's delete files.zip, and confirm we can't load the index
