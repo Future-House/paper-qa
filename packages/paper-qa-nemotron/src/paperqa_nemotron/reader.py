207f121353233f74ade1dd47ae4a50002e6c7eef
James Braza <jamesbraza@gmail.com>
2025-12-10 08:53:38 -0800

    Larger `nemotron-parse` border size for FigQA PDFs (#1233)

--- packages/paper-qa-nemotron/src/paperqa_nemotron/reader.py
+++ packages/paper-qa-nemotron/src/paperqa_nemotron/reader.py
@@ -27,9 +27,11 @@ from paperqa_nemotron.api import (
 )
 
 WHITE_RGB = (255, 255, 255)
-# On DOI 10.1016/j.neuron.2011.12.023, 36-px was an insufficient border
-# with temperature of 0
-DEFAULT_BORDER_SIZE = 42  # pixels
+# On DOI 10.1016/j.neuron.2011.12.023, 36-px was an insufficient border,
+# then on DOI 10.1111/jnc.13398, 42-px was an insufficient border,
+# then on DOI 10.1016/j.neuron.2011.12.023 (again), 56-px was an insufficient border,
+# all with temperature of 0 and DPI 300
+DEFAULT_BORDER_SIZE = 60  # pixels
 
 
 def pad_image_with_border(
